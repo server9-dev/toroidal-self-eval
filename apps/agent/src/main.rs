66bd8eacb0e40e3d9fcd3d72f3f7eb11555168c1
J. Zane Cook <jzanecook@z90.studio>
2025-01-15 07:43:48 -0600

    fix: Comment

--- apps/agent/src/main.rs
+++ apps/agent/src/main.rs
@@ -9,6 +9,7 @@ async fn index() -> impl Responder {
 async fn main() -> std::io::Result<()> {
     rt::spawn(async {
         loop {
+            // Agent logic will go here, API endpoints will be for querying the agent
             println!("Heartbeat check...");
             tokio::time::sleep(tokio::time::Duration::from_secs(10)).await;
         }
